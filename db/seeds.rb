ce6c504a2f881a2a2715bdadb9b3bb47161b2d4d
Fran Rodrigo <franjrodrigop@gmail.com>
2023-02-22 16:04:49 +0100

    Yelp-MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+five_guys = { name: 'Five Guys', address: 'Neumarkt', category: 'chinese' }
+pizza_hut = { name: 'Pizza Hut', address: 'Friesenplatz', category: 'italian' }
+burger_king =  { name: 'Burger King', address: 'Rudolfplatz', category: 'japanese' }
+mcdonalds =  { name: 'McDonalds', address: 'Zulpicheplatz', category: 'french' }
+bang_bang =  { name: 'Bang Bang', address: 'Hansaring', category: 'belgian' }
+
+[five_guys, pizza_hut, burger_king, mcdonalds, bang_bang].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
